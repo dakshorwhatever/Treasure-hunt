5afaebda1473709fe127b35434719b4d6a08d93b
dakshorwhatever <75436470+dakshorwhatever@users.noreply.github.com>
2021-07-14 19:08:22 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("G A O M N  U S", 100,160)
+    fill("lightblue")
+    text("Hint: A game that was once called Space Mafia", 100,180)
+
+    // add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("N I M E  F R T C A", 400,350)
+fill("lightblue")
+text("Hint: An open world Sandbox game loved by millions", 400,370)
+
+fill("white")
+textSize(15)
+text("O B L X O R", 650,160)
+fill("lightblue")
+text("Hint: This game let's you be anyone you want!", 650,180)
+}
